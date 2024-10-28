8c2b0fa163f1f1f4c55b2567cb08156570133639
Cristina143 <cristina.cisneros143@gmail.com>
2024-10-27 19:52:04 -0700

    first commit

---
+++ lib/data/movies.dart
@@ -0,0 +1,62 @@
+final List<Map<String, dynamic>> movies = [
+  {
+    "id": 1,
+    "title_name": "You're a Big Boy Now",
+    "Genres": "Comedy|Drama",
+    "image": "http://dummyimage.com/162x138.png/ff4444/ffffff"
+  }, 
+  {
+    "id": 2,
+    "title_name": "Braveheart",
+    "Genres": "Action|Drama|War",
+    "image": "http://dummyimage.com/108x189.png/dddddd/000000"
+  }, 
+  {
+    "id": 3,
+    "title_name": "Outbreak",
+    "Genres": "Action|Drama|Sci-Fi|Thriller",
+    "image": "http://dummyimage.com/203x212.png/dddddd/000000"
+  }, 
+  {
+    "id": 4,
+    "title_name": "Penthouse North",
+    "Genres": "Drama|Mystery|Thriller",
+    "image": "http://dummyimage.com/135x198.png/ff4444/ffffff"
+  }, 
+  {
+    "id": 5,
+    "title_name": "That Hamilton Woman",
+    "Genres": "Drama|Romance|War",
+    "image": "http://dummyimage.com/249x166.png/5fa2dd/ffffff"
+  }, 
+  {
+    "id": 6,
+    "title_name": "Winning Streak",
+    "Genres": "Drama",
+    "image": "http://dummyimage.com/190x147.png/5fa2dd/ffffff"
+  }, 
+  {
+    "id": 7,
+    "title_name": "Bachelor in Paradise",
+    "Genres": "Comedy|Romance",
+    "image": "http://dummyimage.com/145x207.png/5fa2dd/ffffff"
+  }, 
+  {
+    "id": 8,
+    "title_name": "Eaux d'artifice",
+    "Genres": "Fantasy",
+    "image": "http://dummyimage.com/246x220.png/ff4444/ffffff"
+  }, 
+  {
+    "id": 9,
+    "title_name": "South, The (Sur, El)",
+    "Genres": "Drama|Romance",
+    "image": "http://dummyimage.com/127x104.png/ff4444/ffffff"
+  }, 
+  {
+    "id": 10,
+    "title_name": "Right America: Feeling Wronged - Some Voices from the Campaign Trail ",
+    "Genres": "Documentary",
+    "image": "http://dummyimage.com/100x127.png/dddddd/000000"
+  }
+];
---
+++ lib/src/listview_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:practica17/data/movies.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({super.key});
+
+  @override
+  _ListViewPage createState() => _ListViewPage();
+}
+
+class _ListViewPage extends State<ListViewPage> {
+  List<Map<String, dynamic>> pelis = [...movies];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Practica 17 - Dismissed'),
+        backgroundColor: Colors.orangeAccent,
+      ),
+      body: ListView.builder(
+        itemCount: pelis.length,
+        itemBuilder: (BuildContext context, int index) {
+          final item = pelis[index];
+          return Slidable(
+            key: ValueKey(item['title_name']),
+            startActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) => onDismissed(index, 'Agregar'),
+                  backgroundColor: Colors.green,
+                  foregroundColor: Colors.white,
+                  icon: Icons.library_add,
+                  label: 'Agregar',
+                ),
+              ],
+            ),
+            endActionPane: ActionPane(
+              motion: const ScrollMotion(),
+              children: [
+                SlidableAction(
+                  onPressed: (context) => onDismissed(index, 'Eliminar'),
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  icon: Icons.cancel_outlined,
+                  label: 'Eliminar',
+                ),
+              ],
+            ),
+            child: ListTile(
+              title: Text(item['title_name']),
+              subtitle: Text(item['Genres']),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  void onDismissed(int index, String accion) {
+    setState(() {
+      final snackBar = SnackBar(content: Text('Acción: $accion'));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+      if (accion == 'Eliminar') {
+        pelis.removeAt(index);
+      }
+      // Additional logic can be added for "Agregar" if needed
+    });
+  }
+}
